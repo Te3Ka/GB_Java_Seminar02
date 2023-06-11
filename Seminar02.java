6c06a3f83468bfe5b4ab158782b05ef10e4cd115
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-06-11 14:53:45 +0300

    Exercise-2
    Выполнено задание 2

--- Seminar02.java
+++ Seminar02.java
@@ -1,16 +1,34 @@
+package GB_Java_Seminar02;
+
 import java.util.Scanner;
 
 public class Seminar02 {
     public static void main(String[] args) {
+        // Задание 1.
         // Написать программу, которая запрашивает на ввод две строки
         // После проверяет, равны ли эти строки по содержан.
-
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Insert string № 1 = ");
         String firstString = scanner.nextLine();
         System.out.print("Insert String № 2 = ");
         String secondString = scanner.nextLine();
-
         System.out.println("Strings " + (firstString.equals(secondString) ? "equals" : "not equals"));
+
+
+        // Задание 2.
+        // Написать программу, которая запрашиваент на ввод две строик
+        // После проверяет, являются ли эти строк равны со вращением друг другу
+        // Одна прямая, вторая такая же, но с конца.
+        System.out.print("Insert string № 1 = ");
+        StringBuilder stringBuilderOne = new StringBuilder(scanner.nextLine());
+        System.out.println(stringBuilderOne);
+        System.out.print("Insert String № 2 = ");
+        StringBuilder stringBuilderTwo = new StringBuilder(scanner.nextLine());
+        System.out.println(stringBuilderTwo);
+        String firstStringBuilder = stringBuilderOne.toString();
+        String secondStringBuilder = stringBuilderTwo.reverse().toString();
+        System.out.println("Strings " + (firstStringBuilder.equals(secondStringBuilder)
+                ? "equals reverse" : "not equals reverse"));
     }
 }
