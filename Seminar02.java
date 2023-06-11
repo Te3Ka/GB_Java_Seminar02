31f0102e54dd6f4ed183e32a97bb78b2ae757718
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-06-11 14:35:17 +0300

    Exercise01
    Выполнено 1 задание

--- Seminar02.java
+++ Seminar02.java
@@ -1,7 +1,16 @@
-package GB_Java_Seminar02;
+import java.util.Scanner;
 
 public class Seminar02 {
     public static void main(String[] args) {
+        // Написать программу, которая запрашивает на ввод две строки
+        // После проверяет, равны ли эти строки по содержан.
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Insert string № 1 = ");
+        String firstString = scanner.nextLine();
+        System.out.print("Insert String № 2 = ");
+        String secondString = scanner.nextLine();
+
+        System.out.println("Strings " + (firstString.equals(secondString) ? "equals" : "not equals"));
     }
 }
